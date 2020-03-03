669ce1adc2520da784e667d7623862bde7fc88a7
Thomas Scheike <ts@biostat.ku.dk>
2020-03-03 08:10:12 +0100

    update

---
+++ src/rvpairs2dimrv.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <complex.h>
+#include "twostage.h"
+
+using namespace arma;
+using namespace Rcpp;
+
+
+RcppExport SEXP RVpairs2DIMRV(SEXP irvs, SEXP inrvs) 
+{
+ IntegerVector nrvs(inrvs);
+ mat rvs  = Rcpp::as<mat>(irvs);
+ int j; 
+
+ rvs.print(); 
+
+for (j=0;j<rvs.n_rows;j++) { 
+	// 2-dimensional array with (2xrandom effects) written as row 
+        int lnrv= nrvs(j)-1; // number of random effects for this cluster 	
+	Rprintf(" %d \n",lnrv); 
+	mat rv= reshape(rvs.row(j),2,lnrv); 
+	rv.print(); 
+        Rprintf("==============================\n"); 
+}
+
+List res; 
+res["mm"]=rvs;
+
+  return(res); 
+}
+
