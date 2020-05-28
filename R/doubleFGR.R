a31886aa254ea1184e23ae9eb4396c73be15fd95
Klaus Holst <klaus@holst.it>
2020-05-28 15:16:20 +0200

    fixed man

--- R/doubleFGR.R
+++ R/doubleFGR.R
@@ -3,103 +3,133 @@
 ##' Double CIF Fine-Gray model with two causes
 ##'
 ##' Estimation based on derived hazards and recursive esitmating euqations.
-##' fits two parametrizations 
-##' 1) 
+##' fits two parametrizations
+##' 1)
 ##'  \deqn{
-##' F_1(t,X) = 1 - \exp( \exp( X^T \beta ) \Lambda_1(t)) 
+##' F_1(t,X) = 1 - \exp( \exp( X^T \beta ) \Lambda_1(t))
 ##' }
-##' and 
+##' and
 ##'  \deqn{
-##' F_2(t,X_2) = 1 - \exp( \exp( X_2^T \beta_2 ) \Lambda_2(t)) 
+##' F_2(t,X_2) = 1 - \exp( \exp( X_2^T \beta_2 ) \Lambda_2(t))
 ##' }
 ##' or restricted version
-##' 2) 
+##' 2)
 ##'  \deqn{
-##' F_1(t,X) = 1 - \exp( \exp( X^T \beta ) \Lambda_1(t)) 
+##' F_1(t,X) = 1 - \exp( \exp( X^T \beta ) \Lambda_1(t))
 ##' }
-##' and 
+##' and
 ##'  \deqn{
 ##' F_2(t,X_2,X) = ( 1 - \exp( \exp( X_2^T \beta_2 ) \Lambda_2(t)) ) (1 - F_1(\infty,X))
 ##' }
 ##'
-##' @param formula formula with 'Event' 
+##' @param formula formula with 'Event'
 ##' @param data data frame
 ##' @param offset offsets for cox model
 ##' @param weights weights for Cox score equations
-##' @param X2 specifies the regression design for second CIF model 
+##' @param X2 specifies the regression design for second CIF model
 ##' @param ... Additional arguments to lower level funtions
 ##' @author Thomas Scheike
 ##' @examples
-##' res <- 0 
+##' res <- 0
 ##' data(bmt)
-##' bmt$age2 <- bmt$age 
+##' bmt$age2 <- bmt$age
 ##' newdata <- bmt[1:19,]
-##' par(mfrow=c(5,3))
-##' 
+##' if (interactive()) par(mfrow=c(5,3))
+##'
 ##' ## same X1 and X2
 ##' pr2 <- doubleFGR(Event(time,cause)~age+platelet,data=bmt,restrict=res)
-##' bplotdFG(pr2,cause=1)
-##' bplotdFG(pr2,cause=2,add=TRUE)
+##' if (interactive()) {
+##'   bplotdFG(pr2,cause=1)
+##'   bplotdFG(pr2,cause=2,add=TRUE)
+##' }
 ##' pp21 <- predictdFG(pr2,newdata=newdata)
 ##' pp22 <- predictdFG(pr2,newdata=newdata,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
 ##' pp21 <- predictdFG(pr2)
 ##' pp22 <- predictdFG(pr2,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
-##' 
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
+##'
 ##' pr2 <- doubleFGR(Event(time,cause)~strata(platelet),data=bmt,restrict=res)
-##' bplotdFG(pr2,cause=1)
-##' bplotdFG(pr2,cause=2,add=TRUE)
+##' if (interactive()) {
+##'   bplotdFG(pr2,cause=1)
+##'   bplotdFG(pr2,cause=2,add=TRUE)
+##' }
 ##' pp21 <- predictdFG(pr2,newdata=newdata)
 ##' pp22 <- predictdFG(pr2,,newdata=newdata,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
 ##' pp21 <- predictdFG(pr2)
 ##' pp22 <- predictdFG(pr2,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
-##' 
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
+##'
 ##' ## different X1 and X2
 ##' pr2 <- doubleFGR(Event(time,cause)~age+platelet+age2,data=bmt,X2=3,restrict=res)
-##' bplotdFG(pr2,cause=1)
-##' bplotdFG(pr2,cause=2,add=TRUE)
+##' if (interactive()) {
+##'   bplotdFG(pr2,cause=1)
+##'   bplotdFG(pr2,cause=2,add=TRUE)
+##' }
 ##' pp21 <- predictdFG(pr2,newdata=newdata)
 ##' pp22 <- predictdFG(pr2,newdata=newdata,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
 ##' pp21 <- predictdFG(pr2)
 ##' pp22 <- predictdFG(pr2,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
-##' 
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
+##'
 ##' ### uden X1
 ##' pr2 <- doubleFGR(Event(time,cause)~age+platelet,data=bmt,X2=1:2,restrict=res)
-##' bplotdFG(pr2,cause=1)
-##' bplotdFG(pr2,cause=2,add=TRUE)
+##' if (interactive()) {
+##'   bplotdFG(pr2,cause=1)
+##'   bplotdFG(pr2,cause=2,add=TRUE)
+##' }
 ##' pp21 <- predictdFG(pr2,newdata=newdata)
 ##' pp22 <- predictdFG(pr2,newdata=newdata,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
 ##' pp21 <- predictdFG(pr2)
 ##' p22 <- predictdFG(pr2,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
-##' 
-##' ### uden X2
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
+##'
+##' ### without X2
 ##' pr2 <- doubleFGR(Event(time,cause)~age+platelet,data=bmt,X2=0,restrict=res)
-##' bplotdFG(pr2,cause=1)
-##' bplotdFG(pr2,cause=2,add=TRUE)
+##' if (interactive()) {
+##'   bplotdFG(pr2,cause=1)
+##'   bplotdFG(pr2,cause=2,add=TRUE)
+##' }
 ##' pp21 <- predictdFG(pr2,newdata=newdata)
 ##' pp22 <- predictdFG(pr2,newdata=newdata,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
 ##' pp21 <- predictdFG(pr2)
 ##' pp22 <- predictdFG(pr2,cause=2)
-##' plot(pp21)
-##' plot(pp22,add=TRUE,col=2)
-##' 
+##' if (interactive()) {
+##'   plot(pp21)
+##'   plot(pp22,add=TRUE,col=2)
+##' }
+##'
 ##' @aliases bplotdFG predictdFG
 ##' @export
 doubleFGR <- function(formula,data,offset=NULL,weights=NULL,X2=NULL,...) {# {{{
@@ -143,10 +173,10 @@ doubleFGR <- function(formula,data,offset=NULL,weights=NULL,X2=NULL,...) {# {{{
   X <- model.matrix(Terms, m)
   if (!is.null(intpos  <- attributes(Terms)$intercept))
     X <- X[,-intpos,drop=FALSE]
-  X2call <- X2; 
+  X2call <- X2;
   if (!is.null(X2)) {
 	 if (X2[1]==0)  X2 <- matrix(nrow=0,ncol=0)
-	 else { X2 <-  X[,X2call,drop=FALSE]; 
+	 else { X2 <-  X[,X2call,drop=FALSE];
                 X <- X[,-X2call,drop=FALSE];
 	 }
   } else X2 <- X
@@ -155,7 +185,7 @@ doubleFGR <- function(formula,data,offset=NULL,weights=NULL,X2=NULL,...) {# {{{
   res <- c(doubleFG01R(X,X2,entry,exit,status,id,strata,offset,weights,strata.name,X2call=X2call,...),
    list(call=cl,model.frame=m,formula=formula,strata.pos=pos.strata,cluster.pos=pos.cluster))
   class(res) <- c("doubleFG")
-  
+
   res
 }# }}}
 
@@ -166,7 +196,7 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
   p1 <- ncol(X);
   p2 <- ncol(X2)
   p <- p1+p2
-  if (missing(beta))  beta <- rep(0,p) 
+  if (missing(beta))  beta <- rep(0,p)
   if (p1==0) X <- cbind(rep(0,length(exit)))
   if (p2==0) X2 <- cbind(rep(0,length(exit)))
   if (is.null(strata)) { strata <- rep(0,length(exit)); nstrata <- 1; strata.level <- NULL; } else {
@@ -178,14 +208,14 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
       strata <- as.integer(factor(strata,labels=seq(nstrata)))-1
     }
   }
-  if (is.null(offset)) offset <- rep(0,length(exit)) 
-  if (is.null(weights)) weights <- rep(1,length(exit)) 
+  if (is.null(offset)) offset <- rep(0,length(exit))
+  if (is.null(weights)) weights <- rep(1,length(exit))
   strata.call <- strata
-  Zcall <- matrix(1,1,1) ## to not use for ZX products when Z is not given 
+  Zcall <- matrix(1,1,1) ## to not use for ZX products when Z is not given
   if (!is.null(Z)) Zcall <- Z
 
   ## possible casewights to use for bootstrapping and other things
-  if (is.null(case.weights)) case.weights <- rep(1,length(exit)) 
+  if (is.null(case.weights)) case.weights <- rep(1,length(exit))
 
   trunc <- (!is.null(entry))
   if (!trunc) entry <- rep(0,length(exit))
@@ -196,9 +226,9 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
       if (is.numeric(id)) id <-  fast.approx(ids,id)-1 else  {
       id <- as.integer(factor(id,labels=seq(nid)))-1
      }
-   } else id <- as.integer(seq_along(entry))-1; 
-   ## orginal id coding into integers 
-   id.orig <- id+1; 
+   } else id <- as.integer(seq_along(entry))-1;
+   ## orginal id coding into integers
+   id.orig <- id+1;
 
    dd <- .Call("FastCoxPrepStrata",entry,exit,status,X,id,trunc,strata,weights,offset,Zcall,case.weights,PACKAGE="mets")
    if (!is.null(X2call))  {
@@ -232,7 +262,7 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
 	      val$nstrata <- dd$nstrata
 	      val$strata <- dd$strata
 	      return(val)
-	  } 
+	  }
 	 with(val,structure(-ploglik,gradient=-gradient,hessian=-hessian))
 	}# }}}
 
@@ -247,7 +277,7 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
           opt <- nlm(obj,beta,...)
           opt$method <- "nlm"
       }
-      cc <- opt$estimate;  
+      cc <- opt$estimate;
       names(cc) <- c(colnames(X),colnames(X2))
       if (!stderr) return(cc)
       val <- c(list(coef=cc),obj(opt$estimate,all=TRUE))
@@ -258,7 +288,7 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
 
   se.cumhaz <- lcumhaz <- lse.cumhaz <- NULL
   II <- NULL
-  ### computes Breslow estimator 
+  ### computes Breslow estimator
   if (cumhaz==TRUE) { # {{{
 	 if (no.opt==FALSE & p!=0) {
                II <- - tryCatch(solve(val$hessian),error=
@@ -272,7 +302,7 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
 ###	 cumhaz <- cbind(jumptimes,cumsumstrata(1/val$S0,strata,nstrata))
 	 cumhaz <- cbind(jumptimes,val$base12$base)
 
-###	 if ((no.opt==FALSE & p!=0)) { 
+###	 if ((no.opt==FALSE & p!=0)) {
 ###	     DLambeta.t <- apply(val$E/c(val$S0),2,cumsumstrata,strata,nstrata)
 ###	     varbetat <-   rowSums((DLambeta.t %*% II)*DLambeta.t)
 ###	 ### covv <-  apply(covv*DLambeta.t,1,sum) Covariance is "0" by construction
@@ -284,7 +314,7 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
 ###	 colnames(cumhaz1)    <- c("time","cumhaz")
 ###	 colnames(cumhaz2)    <- c("time","cumhaz")
 ###	 colnames(se.cumhaz) <- c("time","se.cumhaz")
- } # }}} 
+ } # }}}
  else {cumhaz <- se.cumhaz <- lcumhaz <- lse.cumhaz <- NULL}
 
  II <- - tryCatch(solve(val$hessian),error=
@@ -295,15 +325,15 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
 		strata.call=strata.call, strata.level=strata.level,
                 entry=entry,
                 exit=exit,
-                status=status,                
+                status=status,
                 p=p,
                 X=X,
                 X2=X2,
 		X2call=X2call,
 		offsets=offset,
 		weights=weights,
-                id=id.orig, 
-		opt=opt, 
+                id=id.orig,
+		opt=opt,
 		var=II,
 		cumhaz=cumhaz, se.cumhaz=se.cumhaz,
 		lcumhaz=lcumhaz, lse.cumhaz=lse.cumhaz,
@@ -313,8 +343,8 @@ doubleFG01R <- function(X,X2, entry,exit,status,id=NULL,strata=NULL,offset=NULL,
   res
 }# }}}
 
-## double FG in in R version 
-doubleFGstrataR <- function(beta, X, XX, X2, XX2, Sign, cause, Jumps, strata, nstrata, weights, offsets, ZX, caseweights,restrict) 
+## double FG in in R version
+doubleFGstrataR <- function(beta, X, XX, X2, XX2, Sign, cause, Jumps, strata, nstrata, weights, offsets, ZX, caseweights,restrict)
 {# {{{
 	p=length(beta)
 	p1  <-  ncol(X)
@@ -338,15 +368,15 @@ doubleFGstrataR <- function(beta, X, XX, X2, XX2, Sign, cause, Jumps, strata, ns
 
 	S01 = c(revcumsumstrata(eXb1,strata,nstrata))
 	S02 = c(revcumsumstrata(eXb2,strata,nstrata))
-	E1=apply(eXb1*as.matrix(X),2,revcumsumstrata,strata,nstrata)/S01; 
-	E2=apply(eXb2*as.matrix(X2),2,revcumsumstrata,strata,nstrata)/S02; 
+	E1=apply(eXb1*as.matrix(X),2,revcumsumstrata,strata,nstrata)/S01;
+	E2=apply(eXb2*as.matrix(X2),2,revcumsumstrata,strata,nstrata)/S02;
 	Jumps=Jumps+1
 	causeJ <- cause[Jumps]
 	Jumps1 <- Jumps[causeJ==1]
 	Jumps2 <- Jumps[causeJ==2]
 
-	## both E's, SO's, X same 
-	S0J <- cbind(S01,S02)[Jumps,] 
+	## both E's, SO's, X same
+	S0J <- cbind(S01,S02)[Jumps,]
 	eXbJ <- cbind(eXb1,eXb2)[Jumps,]
 	EJ <- cbind(E1,E2)[Jumps,]
 	XJ <- X[Jumps,]
@@ -354,39 +384,39 @@ doubleFGstrataR <- function(beta, X, XX, X2, XX2, Sign, cause, Jumps, strata, ns
 
 	E1 = E1[Jumps1,,drop=FALSE];
 	E2 = E2[Jumps2,,drop=FALSE];
-        E21=.Call("vecMatMat",E1,E1)$vXZ;  
-        E22=.Call("vecMatMat",E2,E2)$vXZ;  
+        E21=.Call("vecMatMat",E1,E1)$vXZ;
+        E22=.Call("vecMatMat",E2,E2)$vXZ;
 
-	XX21=apply(XX*eXb1,2,revcumsumstrata,strata,nstrata)/S01; 
-	XX22=apply(XX2*eXb2,2,revcumsumstrata,strata,nstrata)/S02; 
+	XX21=apply(XX*eXb1,2,revcumsumstrata,strata,nstrata)/S01;
+	XX22=apply(XX2*eXb2,2,revcumsumstrata,strata,nstrata)/S02;
 	XX21 = XX21[Jumps1,,drop=FALSE];
 	XX22 = XX22[Jumps2,,drop=FALSE];
 
-	weightsJ=weights[Jumps];  
-	caseweightsJ=caseweights[Jumps];  
+	weightsJ=weights[Jumps];
+	caseweightsJ=caseweights[Jumps];
 
 	## compute recursive weights given beta1, beta2
-        base12 <- .Call("cumsumstrataDFGR",weightsJ,S0J,causeJ,strata[Jumps],nstrata,eXbJ) 
+        base12 <- .Call("cumsumstrataDFGR",weightsJ,S0J,causeJ,strata[Jumps],nstrata,eXbJ)
 	if (restrict>0) {
 		for (i in 1:restrict) {
-		   Lam1inf <- tailstrata(strata[Jumps],nstrata); 
+		   Lam1inf <- tailstrata(strata[Jumps],nstrata);
 		   Lam1inf  <- base12$base[Lam1inf,1]
 		   base12 <- .Call("cumsumstrataDFGRestrictR",weightsJ,S0J,causeJ,strata[Jumps],nstrata,eXbJ,Lam1inf);
 	        }
 	}
 
-###	DLam  <-  .Call("DLambetaDFGR",weightsJ,S0J,causeJ,EJ,XJ,strata[Jumps],nstrata,eXbJ); 
+###	DLam  <-  .Call("DLambetaDFGR",weightsJ,S0J,causeJ,EJ,XJ,strata[Jumps],nstrata,eXbJ);
 
 	grad1 = (X[Jumps1,,drop=FALSE]-E1);         ## Score
 	grad2 = (X2[Jumps2,,drop=FALSE]-E2);        ## Score
 
-	### weights for Fg1 an Fg2 models 
-	pow1 <- base12$pow1[causeJ==1] 
-	pow2 <- base12$pow2[causeJ==2] 
+	### weights for Fg1 an Fg2 models
+	pow1 <- base12$pow1[causeJ==1]
+	pow2 <- base12$pow2[causeJ==2]
 	cw1 <- (caseweightsJ*weightsJ)[causeJ==1]
 	cw2 <- (caseweightsJ*weightsJ)[causeJ==2]
-	S012 = S01[Jumps1]/(pow1*cw1);        ## S0 with weights 
-	S022 = S02[Jumps2]/(pow2*cw2);        ## S0 with weights 
+	S012 = S01[Jumps1]/(pow1*cw1);        ## S0 with weights
+	S022 = S02[Jumps2]/(pow2*cw2);        ## S0 with weights
 
 	val =  sum(pow1*cw1*(Xb1[Jumps1]-log(S012)))+
 	       sum(pow2*cw2*(Xb2[Jumps2]-log(S022))); ## Partial log-likelihood
@@ -396,24 +426,24 @@ doubleFGstrataR <- function(beta, X, XX, X2, XX2, Sign, cause, Jumps, strata, ns
 	grad <- c(apply(grad21,2,sum),apply(grad22,2,sum))
 	gradient <- grad
 	if (p1==0) gradient <- grad[-1]
-	if (p2==0) gradient  <-  grad[1:p1] 
+	if (p2==0) gradient  <-  grad[1:p1]
 
 
-	## no weights 
+	## no weights
 ###	val2 = caseweightsJ*weightsJ*val;    ## Partial log-likelihood with weights
 	val2 = val;                          ## Partial log-likelihood with weights
 
- 	hesst1 = -(XX21-E21);               ## hessian contributions in jump times 
-	hesst2 = -(XX22-E22);               ## hessian contributions in jump times 
+ 	hesst1 = -(XX21-E21);               ## hessian contributions in jump times
+	hesst2 = -(XX22-E22);               ## hessian contributions in jump times
 ###	hess  = matrix(apply(hesst,2,sum),p,p);
-	hesst12 = hesst1*(cw1)*pow1;        ## hessian over time with weights 
-	hesst22 = hesst2*(cw2)*pow2;        ## hessian over time with weights 
+	hesst12 = hesst1*(cw1)*pow1;        ## hessian over time with weights
+	hesst22 = hesst2*(cw2)*pow2;        ## hessian over time with weights
 
-	## missing some derivative terms for hessian (due to pow=w(beta,\Lam(beta,t-)) 
+	## missing some derivative terms for hessian (due to pow=w(beta,\Lam(beta,t-))
 
-	## setup hessian matrix 
-	hess12 = matrix(apply(hesst12,2,sum),p1,p1);         ## hessian with weights 
-	hess22 = matrix(apply(hesst22,2,sum),p2,p2);         ## hessian with weights 
+	## setup hessian matrix
+	hess12 = matrix(apply(hesst12,2,sum),p1,p1);         ## hessian with weights
+	hess22 = matrix(apply(hesst22,2,sum),p2,p2);         ## hessian with weights
 	hess2 <- matrix(0,p1+p2,p1+p2)
 	pd2 <- p1
 	p <- length(beta)
@@ -421,9 +451,9 @@ doubleFGstrataR <- function(beta, X, XX, X2, XX2, Sign, cause, Jumps, strata, ns
 	if (p2>0) hess2[(p1+1):p,(p1+1):p] <- hess22
 
 	out=list(jumps=Jumps, ploglik=sum(val2),U=grad2,base12=base12,
-		 gradient=matrix(gradient,1,p), hessian=hess2,  
-		 ##hessianttime=hesst2, S2S0=XX2, 
-		 E=EJ, S0=S0J 
+		 gradient=matrix(gradient,1,p), hessian=hess2,
+		 ##hessianttime=hesst2, S2S0=XX2,
+		 E=EJ, S0=S0J
 		 )
 	return(out)
 }# }}}
@@ -452,17 +482,17 @@ predictdFG <- function(x,cause=1,se=FALSE,times=NULL,...)  {# {{{
 	cumhaz <- x$cumhaz
 	coef <- x$coef
 	p <- x$p
-        X2call <- x$X2call; 
+        X2call <- x$X2call;
 
-	### finding the right coefficients 
+	### finding the right coefficients
 	if (p>0) {
               if (!is.null(X2call))  {
-		 if (X2call[1]==0)  { 
-			 p2 <- 0; p1 <- p; 
+		 if (X2call[1]==0)  {
+			 p2 <- 0; p1 <- p;
 		         cc1 <- 1:p; cc2 <- 0
-		 } else { 
-	             p2 <- length(X2call); 
-	             p1 <- p-p2; 
+		 } else {
+	             p2 <- length(X2call);
+	             p1 <- p-p2;
 		     cc1 <- (1:p)[-X2call]
 		     cc2 <- (1:p)[X2call]
 		     if (p2==0) x$X <- x$X
@@ -470,14 +500,14 @@ predictdFG <- function(x,cause=1,se=FALSE,times=NULL,...)  {# {{{
 		     if (p1!=0 & p2!=0) x$X <- cbind(x$X,x$X2)
 		 }
          } else {
-	         model.frame2 <- x$model.frame 
+	         model.frame2 <- x$model.frame
 		 p1 <- p/2
 		 cc1 <- 1:p1
 		 cc2 <- (p1+1):p
 		 p <- p1
 	 }
 	if (cause==1) {
-	      if (is.null(X2call)) x$coef <- x$coef[cc1] 
+	      if (is.null(X2call)) x$coef <- x$coef[cc1]
 	      else x$coef[cc2] <- 0
 	}
 	if (cause==2) {
@@ -487,9 +517,9 @@ predictdFG <- function(x,cause=1,se=FALSE,times=NULL,...)  {# {{{
 	}
 
 	x$cumhaz <- x$cumhaz[,c(1,cause+1)]
-	
-###	if (p>0) { 
-###           ## sets coefficients to 0 for other cause 
+
+###	if (p>0) {
+###           ## sets coefficients to 0 for other cause
 ###	   print("====================")
 ###	   print(x$coef)
 ###	   print(x$p)
@@ -512,5 +542,3 @@ predictdFG <- function(x,cause=1,se=FALSE,times=NULL,...)  {# {{{
 	}
 	return(pll)
 }# }}}
-
-
