b99c23ae25125ee82b4de47c801968145a16aac3
Thomas Scheike <ts@biostat.ku.dk>
2023-10-13 11:17:15 +0200

    minor

--- R/binregTSR.R
+++ R/binregTSR.R
@@ -56,8 +56,8 @@
 ##' ddf <- mets:::gsim(200,covs=1,null=0,cens=1,ce=2)
 ##' 
 ##' bb <- binregTSR(Event(entry,time,status)~+1+cluster(id),ddf$datat,time=2,cause=c(1),
-##' 		cens.code=0,treat.model0=A0.f~+1, treat.model1=A1.f~A0.f,
-##' 		augmentR1=~X11+X12+TR, augmentR0=~X01+X02,augmentC=~A0+X01+X02+A1+X11+X12+TR,
+##' 		cens.code=0,treat.model0=A0.f~+1,treat.model1=A1.f~A0.f,
+##' 		augmentR1=~X11+X12+TR,augmentR0=~X01+X02,augmentC=~A01+A02+X01+X02+A11t+A12t+X11+X12+TR,
 ##' 		response.code=2)
 ##' summary(bb) 
 ##' @export
@@ -720,7 +720,6 @@ val <- list( riskG.iid=riskG.iid, MGc=MGc, CensAugment.times=Augment.times,
   class(val) <- "binregTSR"
   return(val)
 }# }}}
-
 
 ###binregTSR <- function(formula,data,cause=1,time=NULL,
 ###      cens.code=0,response.code=NULL,
@@ -1466,12 +1465,9 @@ simMultistateII <- function(cumhaz,death.cumhaz,death.cumhaz2,n=NULL,
   return(tall)
   }# }}}
 
-   gsim <- function(n,null=1,cens=NULL,ce=9,covs=1,
-	    beta0=c(0.1,0.5,-0.5),beta1=c(0.4,0.3,0.5,-0.5),betaR=c(-0.3,-0.5,0.5)) 
+gsim <- function(n,null=1,cens=NULL,ce=2,covs=1,
+	    beta0=c(0.1,0.5,-0.5),beta1=c(0.4,0.3,0.5,-0.5),betaR=c(-0.3,-0.5,0.5),betac=c(0.3,0.3)) 
    {# {{{
- 
-	    Count2 <- TR <- NULL
-
    X0 <- matrix(rbinom(2*n,1,0.5),n,2)
    expit <- function(x)  return(1/(1+exp(-x)))
    betaX1 <- 0*c(2.7,-2.7)
@@ -1502,11 +1498,21 @@ simMultistateII <- function(cumhaz,death.cumhaz,death.cumhaz2,n=NULL,
    rrr1 <- exp( cbind(0,X0) %*% betaR)
    rrr2 <- exp( cbind(1,X0) %*% betaR)
    } else {
-	   rr21 <- rr22  <- rr01 <- rr02 <- rr21 <- rr22 <- rr11 <- rr12 <- rrr1 <- rrr2 <- 1
+	   rr21 <- rr22  <- rr01 <- rr02 <- rr11 <- rr12 <- rrr1 <- rrr2 <- 1
    }
    ###
    TR1 <- runif(n)*OR1*rrr1
    TR2 <- runif(n)*OR2*rrr2
+
+      TCR1 <- TR1 
+      TCR2 <- TR2 
+   if (cens!=0)  {
+	   if (cens==2 | cens==1) {
+	      TCR1 <- runif(n)*OR1*ce 
+	      TCR2 <- runif(n)*OR2*ce 
+	   } 
+   }
+   ###
    T10 <- rexp(n)*O10*rr01
    T20 <- rexp(n)*O20*rr02
    ### response if (TR1 < T10) or (TR2 < T20)
@@ -1531,20 +1537,56 @@ simMultistateII <- function(cumhaz,death.cumhaz,death.cumhaz2,n=NULL,
  TT21  <-  (1-R2)*T20 + R2*Tt21
  TT22  <-  (1-R2)*T20 + R2*Tt22
  TTt <- cbind(TT11,TT12,TT21,TT22)
- if (is.null(cens)) cc <- max(TT)+1 else cc <- rexp(n)*ce
+ ###
+   TC10 <- rexp(n)*O10*ce
+   TC20 <- rexp(n)*O20*ce
+   ### response if (TR1 < T10) or (TR2 < T20)
+   RC1 <- (TCR1 < TC10)*1 
+   RC2 <- (TCR2 < TC20)*1 
+   ###
+   TCs11 <- rexp(n)*Os11*exp(-3*TCR1)*ce 
+   TCs12 <- rexp(n)*Os12*exp(-3*TCR1)*ce 
+   TCs21 <- rexp(n)*Os21*exp(-1*TCR2)*ce
+   TCs22 <- rexp(n)*Os22*exp(-1*TCR2)*ce
+   ###
+   TC11 <- TCR1 + TCs11
+   TC12 <- TCR1 + TCs12
+   TC21 <- TCR2 + TCs21
+   TC22 <- TCR2 + TCs22
+  ###   
+  CC <-  (A0==1)*( (1-RC1)*TC10 + RC1*((A1==1)*TC11 + (A1==2)*TC12))+ 
+         (A0==2)*( (1-RC2)*TC20 + RC2*((A1==1)*TC21 + (A1==2)*TC22))  
+ CC11  <-  (1-RC1)*TC10 + RC1*TC11
+ CC12  <-  (1-RC1)*TC10 + RC1*TC12
+ CC21  <-  (1-RC2)*TC20 + RC2*TC21
+ CC22  <-  (1-RC2)*TC20 + RC2*TC22
+ CCt <- cbind(CC11,CC12,CC21,CC22)
+ ##
+### CC  <- rexp(n)*ce* exp((A0-1)*betac[1]+(A1-1)*betac[2])
+ ###
+  if (cens==0) cc <- max(TT)+1 else  {
+	  if (cens==1) cc <- rexp(n)*ce*exp((A0-1)*betac[1])
+	  if (cens==2) cc <- CC
+	  if (cens==3) cc <- CC
+  }
   ###
   time <- pmin(TT,cc)
   status <- (TT<cc)*1
   ###  
-  data <- data.frame(time=time,T10=T10,Tt11=Tt11,Tt12=Tt12,TT=TT,status=status,A1=A1,A0=A0,R1=R1,R2=R2,R=(A0==1)*R1+(A0==2)*R2,id=1:n,TR=(A0==1)*TR1+(A0==2)*TR2)
+  data <- data.frame(time=time,T10=T10,Tt11=Tt11,Tt12=Tt12,TT=TT,status=status,A1=A1,A0=A0,R1=R1,R2=R2,R=(A0==1)*R1+(A0==2)*R2,id=1:n,TR=(A0==1)*TR1+(A0==2)*TR2,Resp=(A0==1)*R1+(A0==2)*R2)
   dfactor(data) <- A0.f~A0
   dfactor(data) <- A1.f~A1
-  datat <- timereg::event.split(data,cuts="TR",name.start="entry")
+  datat <- event.split(data,cuts="TR",name.start="entry")
 
   datat <- dtransform(datat,status=2,time==TR)
   datat  <-  count.history(datat,types=2)
   datat  <-  dtransform(datat,A1=0,Count2==0)
   datat$response <- (datat$Count2==1)*1
+  datat$A11t <- (datat$A1==1)*1
+  datat$A12t <- (datat$A1==2)*1
+  datat$A01 <- (datat$A0==1)*1
+  datat$A02 <- (datat$A0==2)*1
+
 
   covX0 <- X0[datat$id,]
   colnames(covX0) <- c("X01","X02")
@@ -1555,7 +1597,7 @@ simMultistateII <- function(cumhaz,death.cumhaz,death.cumhaz2,n=NULL,
   datat <- cbind(datat,covX0,covX1)
   datat <- dtransform(datat,TR=0,Count2==0) 
 
-  res <- list(data=data,datat=datat,TTt=TTt)
+  res <- list(data=data,datat=datat,TTt=TTt,CCt=CCt)
   return(res)
   } # }}}
 
