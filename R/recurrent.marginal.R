3631c4aadd7ec616cbcb7861080a18ad4a56c39c
Thomas Scheike <ts@biostat.ku.dk>
2025-05-26 10:04:19 +0200

    one more example cooking

--- R/recurrent.marginal.R
+++ R/recurrent.marginal.R
@@ -62,7 +62,7 @@
 recurrentMarginal <- function(formula,data,cause=1,...,death.code=2)
 {# {{{
   if (inherits(formula,"phreg")) { # Fall-back to recurrentMarginalPhreg for backward compatibility <= 1.3.5
-  if (inherits(data,"phreg")) return(recurrentMarginalPhreg(formula,data,...))
+  if (inherits(data,"phreg")) return(recurrentMarginalPhreg(formula,data,...)) else stop("You can give 2 phreg objects \n"); 
   }
   cl <- match.call()
   m <- match.call(expand.dots = TRUE)[1:3]
