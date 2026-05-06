669f9f24f62496d7324ab780fd4562e2681b4a0c
Thomas Scheike <ts@biostat.ku.dk>
2026-05-06 08:25:20 +0200

    times also printed when given

--- R/cifreg.R
+++ R/cifreg.R
@@ -200,10 +200,10 @@ print.predictcifreg <- function(x,...) {# {{{
 ret <- summary.predictrecreg(x,...)
 cat("Predictions displayed, for rows:\n")
 print(ret$rows)
-if (!is.null(x$call.times))  {
-  cat("t- Predictions based on predict object, for times:\n")
-  print(x$times)
-}
+###if (!is.null(x$call.times))  {
+###  cat("t- Predictions based on predict object, for times:\n")
+###  print(x$times)
+###}
 return(ret)
 }# }}}
 
