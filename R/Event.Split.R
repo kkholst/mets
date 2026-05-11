f53bf5d6b6072fe059dc55a1a024c3b5e99e6b70
Thomas Scheike <ts@biostat.ku.dk>
2026-05-11 13:26:36 +0200

    up

--- R/Event.Split.R
+++ R/Event.Split.R
@@ -1,58 +1,58 @@
-#' event_split (SurvSplit).
-#' 
-#' Constructs start stop formulation of event time data after a variable in
-#' the data.set. Similar to SurvSplit of the survival package but can also
-#' split after random time given in data frame.
-#' 
-#' 
-#' @param data data to be split
-#' @param time time variable.
-#' @param status status variable.
-#' @param cuts cuts variable or numeric cut (only one value)
-#' @param name.id name of id variable.
-#' @param name.start name of start variable in data, start can also be numeric "0"
-#' @param cens.code code for the censoring.
-#' @param order.id order data after id and start.
-#' @param time.group make variable "before"."cut" that keeps track of wether start,stop is before (1) or after cut (0).
-#' @author Thomas Scheike
-#' @keywords survival
-#' @examples
-#' 
-#' set.seed(1)
-#' d <- data.frame(event=round(5*runif(5),2),start=1:5,time=2*1:5,
-#' 		status=rbinom(5,1,0.5),x=1:5)
-#' d
-#' 
-#' d0 <- event_split(d,cuts="event",name.start=0)
-#' d0
-#' 
-#' dd <- event_split(d,cuts="event")
-#' dd
-#' ddd <- event_split(dd,cuts=3.5)
-#' ddd
-#' event_split(ddd,cuts=5.5)
-#' 
-#' ### successive cutting for many values 
-#' dd <- d
-#' for  (cuts in seq(2,3,by=0.3)) dd <- event_split(dd,cuts=cuts)
-#' dd
-#' 
-#' ###########################################################################
-#' ### same but for situation with multiple events along the time-axis
-#' ###########################################################################
-#' d <- data.frame(event1=1:5+runif(5)*0.5,start=1:5,time=2*1:5,
-#' 		status=rbinom(5,1,0.5),x=1:5,start0=0)
-#' d$event2 <- d$event1+0.2
-#' d$event2[4:5] <- NA 
-#' d
-#' 
-#' d0 <- event_split(d,cuts="event1",name.start="start",time="time",status="status")
-#' d0
-#' ###
-#' d00 <- event_split(d0,cuts="event2",name.start="start",time="time",status="status")
-#' d00
-#' 
-#' @export
+##' event_split (SurvSplit).
+##' 
+##' Constructs start stop formulation of event time data after a variable in
+##' the data.set. Similar to SurvSplit of the survival package but can also
+##' split after random time given in data frame.
+##' 
+##' 
+##' @param data data to be split
+##' @param time time variable.
+##' @param status status variable.
+##' @param cuts cuts variable or numeric cut (only one value)
+##' @param name.id name of id variable.
+##' @param name.start name of start variable in data, start can also be numeric "0"
+##' @param cens.code code for the censoring.
+##' @param order.id order data after id and start.
+##' @param time.group make variable "before"."cut" that keeps track of wether start,stop is before (1) or after cut (0).
+##' @author Thomas Scheike
+##' @keywords survival
+##' @examples
+##' 
+##' set.seed(1)
+##' d <- data.frame(event=round(5*runif(5),2),start=1:5,time=2*1:5,
+##' 		status=rbinom(5,1,0.5),x=1:5)
+##' d
+##' 
+##' d0 <- event_split(d,cuts="event",name.start=0)
+##' d0
+##' 
+##' dd <- event_split(d,cuts="event")
+##' dd
+##' ddd <- event_split(dd,cuts=3.5)
+##' ddd
+##' event_split(ddd,cuts=5.5)
+##' 
+##' ### successive cutting for many values 
+##' dd <- d
+##' for  (cuts in seq(2,3,by=0.3)) dd <- event_split(dd,cuts=cuts)
+##' dd
+##' 
+##' ###########################################################################
+##' ### same but for situation with multiple events along the time-axis
+##' ###########################################################################
+##' d <- data.frame(event1=1:5+runif(5)*0.5,start=1:5,time=2*1:5,
+##' 		status=rbinom(5,1,0.5),x=1:5,start0=0)
+##' d$event2 <- d$event1+0.2
+##' d$event2[4:5] <- NA 
+##' d
+##' 
+##' d0 <- event_split(d,cuts="event1",name.start="start",time="time",status="status")
+##' d0
+##' ###
+##' d00 <- event_split(d0,cuts="event2",name.start="start",time="time",status="status")
+##' d00
+##' 
+##' @export
 event_split <- function(data,
 		time="time",status="status",cuts="cuts",name.id="id",
 		name.start="start", cens.code=0,order.id=TRUE, time.group=FALSE)
